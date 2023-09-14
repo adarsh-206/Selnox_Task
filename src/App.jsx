82e14719994be941b5989911643e2c8840f3206d
adarsh-206 <adarshdhote8624@gmail.com>
2023-09-14 15:16:58 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> I am Adarsh
+          Edit <code>src/App.jsx</code> I am Adarsh Dhote
         </p>
       </div>
       <p className="read-the-docs">
